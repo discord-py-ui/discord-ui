1fc1a525a620d79b91d08dde826fbe550ca40b3f
kuso-senpai <kuso-senpai@users.noreply.github.com>
2021-06-18 16:14:12 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import version
 
 
 def getVersion():
-    with open("discord_py_buttons/__init__.py") as f:
+    with open("discord_py_buttons/__init__.py", "r", encoding="utf-8") as f:
         return [f for f in f.readlines() if f.startswith("__version__")][0].split('"')[1].split('"')[0]
-
+def getReadme():
+    with open("./README.md", "r", encoding="utf-8") as f:
+        return f.read()
 
 setuptools.setup(
-    name="discord_py_buttons",
+    name="discord-py-buttons",
     version=getVersion(),
-    author="RedstoneZockt, 404kuso",
-    author_email="info@redstonezockt.de, bellou9022@gmail.com",
+    author="404kuso, RedstoneZockt, ",
+    author_email="bellou9022@gmail.com, info@redstonezockt.de",
     description="A discord button handler for discord.py",
-    long_description="This is an extension for the discord libary to send, receive and handle buttons",
+    long_description=getReadme(),
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/KusoRedsto/discord_py_buttons",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     classifiers=[
