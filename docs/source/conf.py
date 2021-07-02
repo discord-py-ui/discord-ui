b402839222b2faf01e70135a0fc23381764dc447
kuso-senpai <bellou9022@gmail.com>
2021-07-02 14:45:22 +0200

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-def setup(app):
-    app.add_css_file('theme_overrides.css')
