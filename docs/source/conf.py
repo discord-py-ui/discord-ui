6c9059a4cdf0dedbd0666ee2b486db89122769e5
kuso-senpai <bellou9022@gmail.com>
2021-07-02 15:25:53 +0200

    fixed doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, 404kuso, RedstoneZockt'
 author = '404kuso, RedstoneZockt'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.2'
+release = '1.2.1'
 
 
 # -- General configuration ---------------------------------------------------
