44f1b16bc52230d23847571dae8fd64bb4694786
kuso-senpai <kuso-senpai@users.noreply.github.com>
2021-08-09 22:55:02 +0200

    updated docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, 404kuso, RedstoneZockt'
 author = '404kuso, RedstoneZockt'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
