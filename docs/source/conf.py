4830b6625f51edc43fe5fe89ceb33fdb6d93b826
kuso-senpai <kuso-senpai@users.noreply.github.com>
2021-08-21 01:04:31 +0200

    fixed some doc issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../discord_ui'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
