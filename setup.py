6cd04dea46359b82657345c1f12b976bd420ac1b
RedstoneZockt <45714458+RedstoneZockt@users.noreply.github.com>
2021-09-02 14:30:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,10 +36,10 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+#        'Programming Language :: Python :: 3.6'
+#        'Programming Language :: Python :: 3.7',
+#        'Programming Language :: Python :: 3.8',
+#        'Programming Language :: Python :: 3.9',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
